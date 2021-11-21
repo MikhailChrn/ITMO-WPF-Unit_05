e910b5d60154cf58f9932a1ad68a0b9814693e5d
Михаил <mikhail.cherneyev@yandex.ru>
2021-11-21 16:47:07 +0400

    Добавьте файлы проекта.

---
+++ Unit_05_5/MainWindow.xaml.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Unit_05_5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItem_Click_Close(object sender, RoutedEventArgs e)
+        {
+            textBox.Clear();
+        }
+
+
+
+
+        //Тип шрифта
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+        //Размер шрифта
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox != null)
+            {
+                textBox.FontSize = Convert.ToDouble(fontSize);
+            }
+        }
+
+        //Тумблеры типа Button для переключение параметров шрифта
+        bool isBolt = false;
+        bool isItalic = false;
+        bool isUnder = false;
+
+        private void Bolt_Click(object sender, RoutedEventArgs e)
+        {
+            if (isBolt == false)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+                isBolt = true;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Regular;
+                isBolt = false;
+            }
+        }
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+            if (isItalic == false)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+                isItalic = true;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+                isItalic = false;
+            }
+        }
+        private void Under_Click(object sender, RoutedEventArgs e)
+        {
+            if (isUnder == false)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+                isUnder = true;
+            }
+            else
+            {
+                textBox.TextDecorations = null;
+                isUnder = false;
+            }
+        }
+        //Переключение цвета шрифта типа RadioButton
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = Brushes.Red;
+            }
+        }
+
+    }
+}
---
+++ Unit_05_6/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Unit_05_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Save(object sender, RoutedEventArgs e)
+        //Заимствовано с сайта https://stackoverflow.com !!!!
+        //Как работает практически не понятно !!!
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "savedimage"; // Default file name
+            dlg.DefaultExt = ".jpg"; // Default file extension
+            dlg.Filter = "Image (.jpg)|*.jpg"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                //get the dimensions of the ink control
+                int margin = (int)this.incCanvas.Margin.Left;
+                int width = (int)this.incCanvas.ActualWidth - margin;
+                int height = (int)this.incCanvas.ActualHeight - margin;
+                //render ink to bitmap
+                RenderTargetBitmap rtb =
+                new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+                rtb.Render(incCanvas);
+
+                using (FileStream fs = new FileStream(filename, FileMode.Create))
+                {
+                    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(rtb));
+                    encoder.Save(fs);
+                }
+            }
+        }
+
+        private void MenuItem_Clear(object sender, RoutedEventArgs e)
+        //Очистка рабочего экрана
+        {
+            incCanvas.Strokes.Clear();
+        }
+
+        private void MenuItem_Close(object sender, RoutedEventArgs e)
+        //Закрытие приложения
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Блок выбора цветов
+        private void Color_Black(object sender, RoutedEventArgs e)
+        {
+            incCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+        private void Color_White(object sender, RoutedEventArgs e)
+        {
+            incCanvas.DefaultDrawingAttributes.Color = Colors.White;
+        }
+        private void Color_Red(object sender, RoutedEventArgs e)
+        {
+            incCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+        private void Color_Blue(object sender, RoutedEventArgs e)
+        {
+            incCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+        private void Color_Green(object sender, RoutedEventArgs e)
+        {
+            incCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+
+    }
+}
